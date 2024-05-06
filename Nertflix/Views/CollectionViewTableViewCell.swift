6f8d8146b6ac48f5b9b4b8de4f25ef0290230ea5
huonggiangtran <huonggiangtran2003@gmail.com>
2024-05-06 08:44:45 +0700

    Initial Commit

---
+++ Nertflix/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Nertflix
+//
+//  Created by Lily Tran on 6/5/24.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
