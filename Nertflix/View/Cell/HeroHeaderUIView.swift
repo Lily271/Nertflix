6be6c8c0c9d9c609514bea1432a8eaeb0c7ce9d4
huonggiangtran <huonggiangtran2003@gmail.com>
2024-05-28 08:45:42 +0700

    update

--- Nertflix/View/Cell/HeroHeaderUIView.swift
+++ Nertflix/View/Cell/HeroHeaderUIView.swift
@@ -6,19 +6,21 @@
 //
 
 import UIKit
+protocol HeroHeaderUIViewDelegate: AnyObject {
+    func onPlay(viewModel: TitlePreviewViewModel)
+}
 
 class HeroHeaderUIView: UIView {
+    var model : Title?
+    weak var delegate: HeroHeaderUIViewDelegate?
     
     private let downloadButton: UIButton = {
-        
         let button = UIButton()
         button.setTitle("Download", for: .normal)
         button.layer.borderColor = UIColor.white.cgColor
         button.layer.borderWidth = 1
         button.translatesAutoresizingMaskIntoConstraints = false // to use constrain
         button.layer.cornerRadius = 5
-        
-        
         return button
     }()
     
@@ -58,6 +60,8 @@ class HeroHeaderUIView: UIView {
         addGradient()
         addSubview(playButton)
         addSubview(downloadButton)
+        downloadButton.addTarget(self, action: #selector(onDownload), for: .touchUpInside)
+        playButton.addTarget(self, action: #selector(onPlay), for: .touchUpInside)
         applyConstraints()
     }
     
@@ -79,6 +83,15 @@ class HeroHeaderUIView: UIView {
         NSLayoutConstraint.activate(downloadButtonConstraints)
     }
     
+    public func configure(with model: Title) {
+        self.model = model
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500\(model.poster_path ?? "")") else {
+            return
+        }
+        
+        heroImageView.sd_setImage(with: url, completed: nil)
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         heroImageView.frame = bounds
@@ -86,6 +99,43 @@ class HeroHeaderUIView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError()
+    }
+    
+    @objc func onDownload (sender: UIButton) {
+        if let model = self.model {
+            DataPersistenceManager.shared.downloadTitleWith(model: model) { result in
+                switch result {
+                case .success():
+                    NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    @objc func onPlay (sender: UIButton) {
+        if let title = self.model {
+            guard let titleName = title.original_title ?? title.original_name else {
+                return
+            }
+            
+            
+            APICaller.shared.getMovie(with: titleName + " trailer") { [weak self] result in
+                switch result {
+                case .success(let videoElement):
+                    guard let titleOverview = title.overview else {
+                        return
+                    }
+                    let viewModel = TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: titleOverview)
+                    self?.delegate?.onPlay(viewModel: viewModel)
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+                
+            }
+        }
     }
     
 }
